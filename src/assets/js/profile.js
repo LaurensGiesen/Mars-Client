fd19fe1402fdd79db4ecf8c4059ba80569d96df3
Annelin De Gols <annelin.de.gols@student.howest.be>
2020-11-26 14:52:52 +0100

    code smells van profile opgelost

--- src/assets/js/profile.js
+++ src/assets/js/profile.js
@@ -1,21 +1,21 @@
-"use strict"
+"use strict";
 
 document.addEventListener("DOMContentLoaded", run);
 
 function run() {
     document.querySelector("#change > table > tbody > tr:nth-child(1) > th:nth-child(2) > a")
-        .addEventListener("click", openPopupSubscription)
-    document.querySelector(".close").addEventListener("click", closePopupSubscription)
+        .addEventListener("click", openPopupSubscription);
+    document.querySelector(".close").addEventListener("click", closePopupSubscription);
 }
 
 function openPopupSubscription(e) {
     e.preventDefault();
-    document.getElementById("popup").style.display = "flex"
+    document.getElementById("popup").style.display = "flex";
 
 }
 
 function closePopupSubscription(e) {
     e.preventDefault();
-    document.getElementById("popup").style.display = "none"
+    document.getElementById("popup").style.display = "none";
 }
 
