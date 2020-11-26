fc275ba408e3bcbbbb5beb3cd541348de6d22ae6
cedric.puystjens <cedric.puystjens@student.howest.be>
2020-11-26 18:08:42 +0100

    CP: click on continue goes to registration

---
+++ src/assets/js/login.js
@@ -0,0 +1,13 @@
+"use strict"
+
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    document.querySelector("#continueButton").addEventListener("click", continueButton)
+}
+
+function continueButton(e) {
+    e.preventDefault();
+    window.location.href = "registration.html"
+}
+
