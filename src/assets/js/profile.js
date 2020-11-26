7eaa1e5c00c652ddd5283ed8f00155815217ed0a
cedric.puystjens <cedric.puystjens@student.howest.be>
2020-11-26 11:04:14 +0100

    CP: popup info subs closes when clicked

--- src/assets/js/profile.js
+++ src/assets/js/profile.js
@@ -5,11 +5,17 @@ document.addEventListener("DOMContentLoaded", run);
 function run() {
     document.querySelector("#change > table > tbody > tr:nth-child(1) > th:nth-child(2) > a")
         .addEventListener("click", openPopupSubscription)
+    document.querySelector(".close").addEventListener("click", closePopupSubscription)
 }
 
 function openPopupSubscription(e) {
     e.preventDefault();
     document.getElementById("popup").style.display = "flex"
 
+}
+
+function closePopupSubscription(e) {
+    e.preventDefault();
+    document.getElementById("popup").style.display = "none"
 }
 
