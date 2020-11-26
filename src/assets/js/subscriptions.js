75d6fb14b8f9ce98010ea77017318d1af4be4591
cedric.puystjens <cedric.puystjens@student.howest.be>
2020-11-26 18:34:10 +0100

    CP: add <script> to html + basic js setup

---
+++ src/assets/js/subscriptions.js
@@ -0,0 +1,11 @@
+"use strict"
+
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    document.querySelector("main").addEventListener("click", selectSubscription)
+}
+
+function selectSubscription() {
+
+}
