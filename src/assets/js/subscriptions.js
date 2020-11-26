7b322a2a799e47aa8da0bfc8da73a9daa1f8f608
cedric.puystjens <cedric.puystjens@student.howest.be>
2020-11-26 19:30:46 +0100

    CP: add function to select subscription type

--- src/assets/js/subscriptions.js
+++ src/assets/js/subscriptions.js
@@ -6,6 +6,15 @@ function init() {
     document.querySelector("main").addEventListener("click", selectSubscription)
 }
 
-function selectSubscription() {
+function selectSubscription(e) {
+    e.preventDefault();
+    let target = e.target;
+    if (target === document.querySelector("#subscriptionFree")) {
+        console.log("clicked subscription Free")
+    } else if (target === document.querySelector("#subscriptionBasic")) {
+        console.log("clicked subscription Basic")
+    } else if (target === document.querySelector("#subscriptionPremium")) {
+        console.log("clicked subscription Premium")
+    }
 
 }
