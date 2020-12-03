e3c387ffe87c27dbcabae974d6d8613d1ad19a30
cedric.puystjens <cedric.puystjens@student.howest.be>
2020-12-03 12:31:41 +0100

    CP: made everything clickable

--- src/assets/js/subscriptions.js
+++ src/assets/js/subscriptions.js
@@ -4,18 +4,12 @@ document.addEventListener("DOMContentLoaded", init);
 
 function init() {
     document.querySelector("main").addEventListener("click", selectSubscription)
-    // document.querySelectorAll("li").forEach(element => element.addEventListener("click", selectSubscription2))
 }
 
-// function selectSubscription2(e) {
-//     e.preventDefault();
-//     let test = e.target.closest("section")
-//     console.log(test)
-// }
 
 function selectSubscription(e) {
     e.preventDefault();
-    let selectedSubscription = e.target;
+    let selectedSubscription = e.target.closest("section",);
     if (selectedSubscription === document.querySelector("#subscriptionFree") ||
         selectedSubscription === document.querySelector("#subscriptionBasic") ||
         selectedSubscription === document.querySelector("#subscriptionPremium")) {
