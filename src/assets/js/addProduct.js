b823b3522ac325c7f3f9838a49efeca483087929
Laurens Giesen <laurens.giesen@student.howest.be>
2020-12-08 17:57:45 +0100

    fixing pipeline(making html valid) 4.0

--- src/assets/js/addProduct.js
+++ src/assets/js/addProduct.js
@@ -3,7 +3,30 @@
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
+    addDateInForm();
     document.querySelector('#addProduct').addEventListener('click', addProduct);
+}
+
+function addDateInForm() {
+    document.querySelector('div').innerHTML += `<label for="owner">Please enter your name:</label>
+        <input type="text" id="owner" name="name" placeholder="Owner" required>
+
+        <label for="name">Name product:</label>
+        <input type="text" id="name" name="name" placeholder="Apple" required>
+
+        <label for="date">Date of the day:</label>
+        <input type="date" id="date" name="date" min="2020-12-08" required>
+        
+        <label for="price">Price:</label>
+        <input type="number" id="price" required min="1" value="1" step="1">
+
+        <label for="amount">Amount:</label>
+        <input type="number" id="amount" required min="1" value="1" step="1">
+
+        <label for="picture">Picture:</label>
+        <input type="file" id="picture" required>
+
+        <input id="addProduct" type="submit" value="Add">`
 }
 
 function addProduct(e) {
