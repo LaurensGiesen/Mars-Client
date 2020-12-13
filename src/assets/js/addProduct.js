5275a02cc0177a9a980240c604ca715e01a9598c
TimoDC <timo.de.clercq@student.howest.be>
2020-12-13 01:24:32 +0100

    img bug fixed

--- src/assets/js/addProduct.js
+++ src/assets/js/addProduct.js
@@ -36,18 +36,29 @@ function addProduct(e) {
     let date = document.querySelector('#date').value;
     let price = parseInt(document.querySelector('#price').value);
     let amount = parseInt(document.querySelector('#amount').value);
-    let img = document.querySelector('#picture').value;
+    let img = document.querySelector('#picture');
+    let file = img.files[0];
+    let reader = new FileReader();
+    reader.onloadend = function() {
+        let newProduct = JSON.stringify({
+            name: name,
+            image: reader.result,
+            price: price,
+            ownerId: 1,
+            date: date,
+            amount: amount,
+            type: "plant"
+        });
+        console.log(newProduct)
+        apiCall("addProduct", "POST", newProduct).then(() => document.location.href = "marketplace.html");
+    }
+    reader.readAsDataURL(file);
 
-    let newProduct = JSON.stringify({
-        name: name,
-        image: img,
-        price: price,
-        ownerId: 1,
-        date: date,
-        amount: amount,
-        type: "plant"
-    });
-    apiCall("addProduct", "POST", newProduct).then(() => document.location.href = "marketplace.html");
+}
+
+function encodeImageFileAsURL(element) {
+
+
 
 }
 
